3b9f4796c2baaac9eb33df1a5851d979ced73c52
oguzcicek <soaoflisa@hotmail.com>
2023-02-21 22:13:58 +0300

    localhost url changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost:3001' }
   config.hosts = [
+    "127.0.0.1",
     "localhost",
     "localhost:3001",
     "admin.clubgamefi.com"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.hosts = [
+    "127.0.0.1",
     "localhost",
     "localhost:3001",
     "admin.clubgamefi.com"
