0a485f1e22ae8ac65e53ff9c338c6004ce375508
oguzcicek <soaoflisa@hotmail.com>
2023-02-21 21:20:46 +0300

    env config file edited

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << "clubgamefi.com"
+  config.hosts << /application\.local/
+
 end
