16c5128dce4eb6bbb8460de99346f0c1f74383f1
oguzcicek <soaoflisa@hotmail.com>
2023-11-10 13:23:51 +0300

    cors changed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #origins '*'
-        origins 'localhost:3000', '127.0.0.1:3000', 'localhost:3001', '127.0.0.1:3001',
-            /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins '*'
+    headers: :any,
+    resource '*', 
+    headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
