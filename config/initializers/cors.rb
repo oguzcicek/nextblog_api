57ecba6f3703edb33c4a5b89696465f35a6e754e
oguzcicek <soaoflisa@hotmail.com>
2023-11-12 12:42:13 +0300

    A

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #origins '*'
-    origins '*'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['X-Total-Count']
+     origins '*'
+     resource '*',
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+       expose: ['X-Total-Count']
   end
 end
